946bb59728c384f41ffa46cec9465f19baeceeeb
akorobov <akorobov@itquick.ru>
2024-08-04 19:56:32 +0200

    init App

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+    console.log('123');
+    return <>It works!!!</>
+};
+
+export default App;
--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,6 @@
-const webpack = require('webpack');
 const path = require('path');
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const dotenv = require('dotenv');
-
-// Загружаем переменные окружения из .env файла
-const env = dotenv.config().parsed;
-
-const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-}, {});
 
 module.exports = {
     entry: './src/index.tsx',
@@ -39,10 +28,6 @@ module.exports = {
                 ]
             },
             {
-                test: /\.svg$/,
-                use: ['@svgr/webpack', 'url-loader']
-            },
-            {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource'
             }
@@ -56,31 +41,30 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
-        new MonacoWebpackPlugin({
-            customTypeDefinitions: {
-                react: 'node_modules/@types/react/index.d.ts'
-            },
-            languages: ['javascript', 'java', 'html', 'typescript'],
-            features: ['!gotoSymbol']
-        }),
         new HtmlWebpackPlugin({
-            template: './public/index.html',
-            favicon: './public/fav.svg'
+            template: './public/index.html'
+            //favicon: './public/fav.svg'
         }),
         new CopyPlugin({
             patterns: [
-                { from: 'public/manifest.json', to: 'manifest.json' },
-                { from: 'public/fav.svg', to: 'fav.svg' },
-                { from: 'public/web.config', to: 'web.config' },
-                { from: 'node_modules/@types/react/index.d.ts', to: 'path/to/@types/react/index.d.ts' }
+                { from: 'public/manifest.json', to: 'manifest.json' }
             ]
-        }),
-        new webpack.DefinePlugin(envKeys)
+        })
     ],
     devServer: {
         historyApiFallback: true,
         static: path.join(__dirname, 'dist'),
         compress: true,
         port: 8000
-    }
+    },
+    ignoreWarnings: [
+        {
+            module: /node_modules\/bootstrap\/scss\/_type.scss/,
+            message: /Deprecation Warning/
+        },
+        {
+            module: /node_modules\/bootstrap\/scss\/_reboot.scss/,
+            message: /Deprecation Warning/
+        }
+    ]
 };
